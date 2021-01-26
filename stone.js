36a695ea32740b8d73c9e70cfc31663f73b0447f
hanishka2009 <74358514+hanishka2009@users.noreply.github.com>
2021-01-26 16:23:39 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,25 @@
+class stone {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic:false,
+          restitution:0,
+          friction:1,
+          density:1.2
+      }
+      
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width =width;
+      this.height=height;
+      this.image =loadImage("stone.png");
+      World.add(world, this.body);
+    }
+    display(){
+        push();
+      var pos =this.body.position;
+      translate(pos.x,pos.y)
+      imageMode(CENTER);
+     
+      image(this.image, 0, 0, this.width,this.height);
+      pop();
+    }
+  }
